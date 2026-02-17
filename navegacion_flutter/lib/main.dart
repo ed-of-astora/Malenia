2e64a102d875a299ccd4663af16522a5970c63d5
Ed Rubio <ed-rubio@outlook.com>
2026-02-16 21:04:48 -0800

    FEAT: Consulta a API

--- navegacion_flutter/lib/main.dart
+++ navegacion_flutter/lib/main.dart
@@ -1,170 +1,170 @@
-// Importaciones:
+
+// -------------------------------------------------------------------------- //
+
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-// Función principal:
+// -- MAIN ------------------------------------------------------------------ //
+
 void main() {
-  runApp(const NavegacionFlutterApp());
+    runApp(const MainApp());
 }
 
-// Widget raíz de la aplicación:
-class NavegacionFlutterApp extends StatelessWidget {
-  const NavegacionFlutterApp({super.key});
+class MainApp extends StatelessWidget {
+    const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Mi primer APP con navegación: Ed Rubio',
-      theme: ThemeData(primarySwatch: Colors.red, useMaterial3: true),
-
-      home: const LoginScreen(),
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+        return const MaterialApp(
+            home: LoginPage(),
+        );
+    }
 }
 
-// Inicio de sesión:
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+// -- LOGIN PAGE ------------------------------------------------------------ //
 
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
+class LoginPage extends StatefulWidget {
+    const LoginPage({super.key});
+
+    @override
+    State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
-  final TextEditingController _nameController = TextEditingController();
+class _LoginPageState extends State<LoginPage> {
+    final TextEditingController _nameController = TextEditingController();
 
-  final String radahnFestival =
-      "Champions, welcome! The stars have aligned! The festival is nigh! General Radahn, mightiest demigod of the Shattering, awaits you! Champions, prepare for battle! Defeat the General, claim glory, and grab that Great Rune! A celebration of war! The Radahn Festival!";
+    @override
+    void dispose() {
+        _nameController.dispose();
+        super.dispose();
+    }
 
-  @override
-  void dispose() {
-    _nameController.dispose();
-    super.dispose();
-  }
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            body: Center(
+                child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'PANTALLA DE INICIO',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 20),
-              Image.asset('assets/ashenOne.png', width: 240),
-              const SizedBox(height: 20),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                            Image.asset('assets/ashenOne.png', width: 240),
+                            const SizedBox(height: 60),
 
-              TextField(
-                controller: _nameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Escribe tu nombre',
-                  labelText: 'Nombre del campeón...',
+                            TextField(
+                                controller: _nameController,
+                                decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Escribe tu nombre',
+                                    labelText: 'And who are you? The proud Lord said...',
+                                ),
+                            ),
+                            const SizedBox(height: 20),
+
+                            ElevatedButton(
+                                onPressed: () {
+                                    String nombre = _nameController.text.trim();
+
+                                    Navigator.push(context,
+                                        MaterialPageRoute(
+                                            builder: (context) => HomePage(
+                                                message: "¡Hola, $nombre!",
+                                            ),
+                                        ),
+                                    );
+                                },
+
+                                child: const Text('CONTINUAR'),
+                            ),
+                        ],
+                    ),
                 ),
-              ),
-              const SizedBox(height: 20),
-
-              ElevatedButton(
-                onPressed: () {
-                  String nombre = _nameController.text.trim();
-
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DetailScreen(
-                        message: "¡Hola, $nombre! \n\n$radahnFestival",
-                      ),
-                    ),
-                  );
-                },
-                child: const Text('CONTINUAR'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-// Pantalla de detalles:
-class DetailScreen extends StatelessWidget {
-  final String message;
-
-  const DetailScreen({super.key, required this.message});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-
-      appBar: AppBar(
-        title: const Text('RADAHN FESTIVAL'),
-        backgroundColor: Colors.transparent,
-        foregroundColor: Colors.white,
-        elevation: 0,
-      ),
-
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Image.asset('assets/ashenOne.png', fit: BoxFit.cover),
-          ),
-
-          Positioned.fill(child: Container(color: Colors.black54)),
-
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 20),
-
-                  Container(
-                    padding: const EdgeInsets.all(25),
-                    decoration: BoxDecoration(
-                      color: Colors.black45,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Text(
-                      message,
-                      style: const TextStyle(
-                        fontSize: 22,
-                        color: Colors.white,
-                        fontStyle: FontStyle.italic,
-                        height: 1.5,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-
-                  const SizedBox(height: 40),
-
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.redAccent.shade700,
-                      foregroundColor: Colors.white,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 30,
-                        vertical: 20,
-                      ),
-                    ),
-                    label: const Text('Regresar. :('),
-                  ),
-                ],
-              ),
             ),
-          ),
-        ],
-      ),
-    );
-  }
+        );
+    }
 }
+
+// -- HOME PAGE ------------------------------------------------------------- //
+
+class HomePage extends StatefulWidget {
+    final String message;
+    const HomePage({super.key, required this.message});
+
+    @override
+    State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+    double? exchangeRate;
+    bool loading = true;
+
+    @override
+    void initState() {
+        super.initState();
+        obtenerTipoCambio();
+    }
+
+    Future<void> obtenerTipoCambio() async {
+        final url = Uri.parse('https://open.er-api.com/v6/latest/USD');
+        final response = await http.get(url); // Petición.
+
+        if (response.statusCode == 200) {
+            final data = jsonDecode(response.body);
+
+            setState(() {
+                exchangeRate = data['rates']['MXN'];
+                loading = false;
+            });
+        } else {
+            setState(() {
+                loading = false;
+            });
+        }
+    }
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            extendBodyBehindAppBar: true,
+
+            appBar: AppBar(
+                title: const Text('Development'),
+                backgroundColor: Colors.transparent,
+                foregroundColor: Colors.white,
+                elevation: 0,
+            ),
+
+            body: Center(child: loading
+                ? const CircularProgressIndicator()
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                        Text(widget.message,
+                            style: const TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
+                        ),
+
+                        const SizedBox(height: 20),
+
+                        Text(exchangeRate != null
+                            ? '1 USD es equivalente a $exchangeRate MXN'
+                            : 'Ha ocurrido un error en la consulta.',
+                                style: const TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                ),
+                                textAlign: TextAlign.center,
+                        ),
+                    ],
+                ),
+            ),
+        );
+    }
+}
+
+// -------------------------------------------------------------------------- //
